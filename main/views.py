654d887e8e6fea13e25560f00bd4f0c5829b8570
tologon01 <77880104+tologon01@users.noreply.github.com>
2021-02-05 20:24:43 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+
+def homepage(request):
+    return HttpResponse("hello world!")
+
+def test(request):
+    return render(request, "test.html")
+
+def product(request):
+    return HttpResponse("This is my first page")
+
